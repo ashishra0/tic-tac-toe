a06d5255fc28c79124b5b93c41dd3dea970a37fa
Ashish Rao <ashishrao2598@gmail.com>
2019-11-22 16:37:40 +0530

    fix indentation

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function turn(id, player) {
 
 function checkWin(board, player) {
   let plays = board.reduce((a, e, i) => 
-                           (e === player) ? a.concat(i) : a, []);
+  (e === player) ? a.concat(i) : a, []);
   let gameWon = null;
   for (let[index, win] of winCombos.entries()) {
     if (win.every(elem => plays.indexOf(elem) > -1)) {
